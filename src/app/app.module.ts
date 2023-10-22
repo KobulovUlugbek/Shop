630086d5b08b78fbae27c9448ab2f8ce0cb429fb
Ulugbek Kobulov <108155304+KobulovUlugbek@users.noreply.github.com>
2023-10-22 14:31:11 +0200

    implement cart logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,6 +26,7 @@ import { FiltersComponent } from './pages/home/components/filters/filters.compon
 import { MatChipsModule } from '@angular/material/chips';
 import { ProductBoxComponent } from './pages/home/components/product-box/product-box.component';
 import { CartComponent } from './pages/cart/cart.component';
+import { CartService } from './services/cart.service';
 
 @NgModule({
   declarations: [
@@ -59,7 +60,7 @@ import { CartComponent } from './pages/cart/cart.component';
 
   ],
   
-  providers: [],
+  providers: [CartService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from '../../services/cart.service';
+import { Product } from 'src/app/models/product.modul';
 
 const ROWS_HEIGHT: { [id: number ]: number } = { 1: 400, 3: 335, 4: 350 };
 
@@ -13,7 +15,7 @@ export class HomeComponent implements OnInit {
   category: string | undefined;
   rowHeight = ROWS_HEIGHT[this.cols];
 
-  constructor() { }
+  constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
   }
@@ -27,4 +29,15 @@ export class HomeComponent implements OnInit {
     this.category = newCategory;
   }
 
+  onAddToCart(product: Product):void{
+    this.cartService.addToCart({
+      product : product.image,
+      name: product.title,
+      price: product.price,
+      quantity: 1,
+      id: product.id
+    });
+  }
+
 }
+

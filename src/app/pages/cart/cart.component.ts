c86d3f0b2ca2bb2158a122e75d95998846f51e36
Ulugbek Kobulov <108155304+KobulovUlugbek@users.noreply.github.com>
2023-10-24 07:24:03 +0200

    install stripe

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { loadStripe } from '@stripe/stripe-js';
 import { Cart, CartItem } from 'src/app/models/cart.model';
 import { CartService } from 'src/app/services/cart.service';
 
@@ -37,7 +39,7 @@ export class CartComponent implements OnInit {
     'action',
   ];
 
-  constructor(private cartService: CartService) {}
+  constructor(private cartService: CartService, private http: HttpClient ) {}
 
   ngOnInit(): void {
     this.cartService.cart.subscribe((_cart: Cart) =>{
@@ -64,5 +66,16 @@ export class CartComponent implements OnInit {
 
   onRemoveQuantity(item: CartItem): void {
     this.cartService.removeQuantity(item);
+  }
+
+  onCheckOut():void {
+    this.http.post('http://localhost:4242/checkout', {
+      items: this.cart.items
+    }).subscribe(async(res: any) =>{
+      let stripe = await loadStripe('pk_test_51O4cfuIFGlYrwlMGQZHvJi03vbd3HTWVwvd8fB5kuHDCd4e3xOo2v6msJb9ByfNMXgWagXJF30290KX0PJQMAlQT00z2OWXGte');
+      stripe?.redirectToCheckout({
+        sessionId: res.id
+      })
+    });
   }
 }
